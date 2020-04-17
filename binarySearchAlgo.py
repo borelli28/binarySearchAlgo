0d1e4d48a73065a094ed83eb99706041fe475569
borelli28 <borelliarmadno12@gmail.com>
2020-04-17 03:37:09 -0400

    Added midpoint() method to find and return the midpoint of the list

--- binarySearchAlgo.py
+++ binarySearchAlgo.py
@@ -3,6 +3,7 @@
 
 class BinarySearchAlgo:
 
+    #Take the list where we will conduct the binary search as a parameter.
     def __init__(self, ls):
         self.ls = ls
 
@@ -16,11 +17,23 @@ class BinarySearchAlgo:
 
         return precious_int_elmt
 
-
+    #Finds the midpoint of the given list.
     def midpoint(self):
-        pass
+
+        # self.ls = [1, 2, 3, 4, 5, 6, 7]   #Keep commented if not debugging! This list is used to test the midpoint() return.
+
+        #get the lenght of the list and divided by two to get the middle point of the list
+        ls_midpoint = len(self.ls) / 2
+
+        #when ls_midpoint divide odd numbers it will give a float number. So this var will use round() to round it to a whole number
+        midpoint_rounded = round(ls_midpoint)
+
+        return int(midpoint_rounded)
 
 
 
 # #test the precious_element() method
 # print(BinarySearchAlgo().precious_element())
+
+# #test the midpoint() method
+# print(BinarySearchAlgo().midpoint())
