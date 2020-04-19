da7f4f061907d1880adf6914c27b0b9d3265f7fb
borelli28 <borelliarmadno12@gmail.com>
2020-04-19 03:50:51 -0400

    Added comments and change variables names to make code more readable.

--- binarySearchAlgo.py
+++ binarySearchAlgo.py
@@ -7,20 +7,20 @@ class BinarySearchAlgo:
         self.counter = 0    #Used to counter how many times the algorithm goes trough the while loop to find the term
 
     #Method take a list and the term we going to search as parameters
-    def bi_search(self, ls, search_term):
+    def bi_search(self, ls, target):
         ls_len = len(ls)
-        l = 0
-        r = ls_len - 1
+        start = 0               #first index of list
+        end = ls_len - 1        #last index on the list
 
 
 
-        while l <= r:
+        while start <= end:
 
-            midpoint = floor((l + r) / 2)
-            if ls[midpoint] < search_term:
-                l = midpoint + 1
+            midpoint = floor((start + end) / 2)
+            if ls[midpoint] < target:
+                start = midpoint + 1
 
-            elif ls[midpoint] > search_term:
+            elif ls[midpoint] > target:
                 r = midpoint -1
 
             else:
@@ -35,7 +35,7 @@ class BinarySearchAlgo:
 # Insert your list here
 list = [1,2,3,4,7,9,12,14,18,20,27,45,100]
 
-#Term to be searched
+#Target
 term = 12
 
 index = BinarySearchAlgo().bi_search(list, term)
